748a75976ae57aa3f334e8ef5b9dc07c1a7455df
Bulat Gafarov <gafaroval@Bulats-MacBook-Pro-3.local>
2017-03-07 14:01:26 -0500

    First commit

---
+++ restrictions.m
@@ -0,0 +1,18 @@
+classdef restrictions
+    
+    
+%%      restrictions class describes restrictions on IRF in SVARS
+%
+%       This version: 3.7.2017
+%       Last edited by Bulat Gafarov 
+%%     
+
+
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
