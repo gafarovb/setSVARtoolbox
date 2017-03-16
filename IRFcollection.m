5546ce89027be0d8df4ef652fe03da03d98541ee
gafaroval <gafaroval@192.168.1.52>
2017-03-16 16:07:16 -0400

    IRF collection object

---
+++ IRFcollection.m
@@ -0,0 +1,25 @@
+classdef IRFcollection < IRF
+    %IRFCOLLECTION Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        
+    end
+    
+    methods
+        function  obj = IRFcollection(IRFmatrix,names,label)
+            if nargin ~= 0
+                n = size(IRFmatrix,1);
+                obj(n) = IRFcollection;
+                for iVar = 1:n
+                    currentIRF = IRF(IRFmatrix(iVar,:),names(iVar),label);
+                    obj(iVar).Val = currentIRF.Val;
+                    obj(iVar).labelTS = names(iVar);
+                    obj(iVar).label = label;
+                end
+            end
+        end
+    end
+    
+end
+
